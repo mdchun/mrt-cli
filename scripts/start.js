e78edf3d434c633d87ab0f5b99d3bdc3302b1c34
madechun <45516694@qq.com>
2019-11-22 00:55:42 +0800

    require -> require.resolve

--- scripts/start.js
+++ scripts/start.js
@@ -1,36 +1,37 @@
-process.env.NODE_ENV = "development";
+process.env.NODE_ENV = 'development'
 
-const webpack = require("webpack");
-const logger = require("../lib/logger");
-const webpackDevServer = require("webpack-dev-server");
+const webpack = require('webpack')
+const logger = require('../lib/logger')
+const webpackDevServer = require('webpack-dev-server')
+const { cwdPath } = require('../lib/util')
+const serverConfig = require('../config/webpack.config.dev')
 
-const serverConfig = require("../config/webpack.config.dev");
-
-const compiler = webpack(serverConfig);
+const compiler = webpack(serverConfig)
 
 const server = new webpackDevServer(compiler, {
-  publicPath: serverConfig.output.publicPath,
-  hot: true,
-  stats: "minimal",
-
-  open: true,
-  historyApiFallback: true
-});
+	publicPath: serverConfig.output.publicPath,
+	hot: true,
+	hotOnly: true,
+	stats: 'minimal',
+	contentBase: cwdPath('./dist'),
+	open: true,
+	historyApiFallback: true
+})
 
 const defaults = {
-  host: "0.0.0.0",
-  port: 8080,
-  https: false
-};
+	host: '0.0.0.0',
+	port: 8080,
+	https: false
+}
 
 server.listen(defaults.port, defaults.host, err => {
-  if (err) {
-    logger.error(err);
-    process.exit(1);
-  }
-  const url = `http://localhost:${defaults.port}`;
-  return logger.log(`Development server started. Visit ${url}`);
-});
+	if (err) {
+		logger.error(err)
+		process.exit(1)
+	}
+	const url = `http://localhost:${defaults.port}`
+	return logger.log(`Development server started. Visit ${url}`)
+})
 
 // 也可用下面方式
 // app.use(
